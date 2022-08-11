28d0e00ec974619d702256154638cc4f6bb606e9
Colin Warner <colinswarner@gmail.com>
2022-08-11 08:44:10 -0700

    Add files via upload

--- county-records/OneSharedStory/app.py
+++ county-records/OneSharedStory/app.py
@@ -83,6 +83,38 @@ df5['Role A'] = 'paid taxes for themselves and their property'
 df5['Year'] = df5['Year'].apply(str)
 
 
+#read in dataset 6, define people, source, event, and roles
+df6 = pd.read_csv("BuckinghamVA1867PPTaxAllRev1.csv")
+df6['Person A'] = df6['First Name(s) as written (ex Wm or Wm J)'] + ' ' + df6['Surname']
+df6['Person B'] = df6['First name(s) of Employer '] + ' ' + df6['Surname of employer of male negroes']
+df6['Source'] = '1867 Buckingham Property Tax'
+df6['Event'] = df6['Person A'] + ' is paying taxes in the year ' + df6['Year'].apply(str)
+df6['Role A'] = 'paid taxes for themselves and their property'
+df6['Role B'] = 'employer of ' + df6['Person A']
+df6['Year'] = df6['Year'].apply(str)
+
+#read in dataset 7, define people, source, event, and roles
+df7 = pd.read_csv("CumberlandVA1867PPTaxRPAllrev1.csv")
+df7['Person A'] = df7['First Name(s) as written (ex Wm or Wm J)'] + ' ' + df7['Surname']
+df7['Person B'] = df7['First name(s) of Employer '] + ' ' + df7['Surname of employer of male negroes']
+df7['Source'] = '1867 Cumberland Property Tax'
+df7['Event'] = df7['Person A'] + ' is paying taxes in the year ' + df7['Year'].apply(str)
+df7['Role A'] = 'paid taxes for themselves and their property'
+df7['Role B'] = 'employer of ' + df7['Person A']
+df7['Year'] = df7['Year'].apply(str)
+
+#read in dataset 8, define people, source, event, and roles
+df8 = pd.read_csv("OrangeVA1867PPTaxAll.csv")
+df8['Person A'] = df8['First Name(s) as written (ex Wm or Wm J)'] + ' ' + df8['Surname']
+df8['Person B'] = df8['First name(s) of Employer '] + ' ' + df8['Surname of employer of male negroes']
+df8['Source'] = '1867 Orange Property Tax'
+df8['Event'] = df8['Person A'] + ' is paying taxes in the year ' + df8['Year'].apply(str)
+df8['Role A'] = 'paid taxes for themselves and their property'
+df8['Role B'] = 'employer of ' + df8['Person A']
+df8['Year'] = df8['Year'].apply(str)
+
+
+
 #turn dataset 1 into json format
 df_json_text = df.to_json(orient="records")
 df_json = json.loads(df_json_text)
@@ -102,6 +134,18 @@ df_json4 = json.loads(df_json_text4)
 #turn dataset 5 into json format
 df_json_text5 = df5.to_json(orient="records")
 df_json5 = json.loads(df_json_text5)
+
+#turn dataset 6 into json format
+df_json_text6 = df6.to_json(orient="records")
+df_json6 = json.loads(df_json_text6)
+
+#turn dataset 7 into json format
+df_json_text7 = df7.to_json(orient="records")
+df_json7 = json.loads(df_json_text7)
+
+#turn dataset 8 into json format
+df_json_text8 = df8.to_json(orient="records")
+df_json8 = json.loads(df_json_text8)
 
 myclient = pymongo.MongoClient("mongodb://localhost/")
 
@@ -134,6 +178,23 @@ louisa1866_collection = story_db["louisa1866_collection"]
 louisa1866_collection.delete_many({})
 louisa1866 = louisa1866_collection.insert_many(df_json5)
 
+# this creates a collection, which is similar to a 'table' in a relational database.
+buckingham1867_collection = story_db["buckingham1867_collection"]
+buckingham1867_collection.delete_many({})
+buckingham1867 = buckingham1867_collection.insert_many(df_json6)
+
+# this creates a collection, which is similar to a 'table' in a relational database.
+cumberland1867_collection = story_db["cumberland1867_collection"]
+cumberland1867_collection.delete_many({})
+cumberland1867 = cumberland1867_collection.insert_many(df_json7)
+
+# this creates a collection, which is similar to a 'table' in a relational database.
+orange1867_collection = story_db["orange1867_collection"]
+orange1867_collection.delete_many({})
+orange1867 = orange1867_collection.insert_many(df_json8)
+
+
+
 
 # EXPORT EACH OF OUR COLLECTIONS TO JSON FILE FOR ONE SHARED STORY TO POTENTIALLY SHARE WITH OTHER ANCESTRY COMPANIES
 
@@ -157,7 +218,17 @@ cursor = louisa1866_collection.find({})
 with open('louisa1866_collection.json', 'w') as file:
     json.dump(json.loads(dumps(cursor)), file)
 
+cursor = buckingham1867_collection.find({})
+with open('buckingham1867_collection.json', 'w') as file:
+    json.dump(json.loads(dumps(cursor)), file)
     
+cursor = cumberland1867_collection.find({})
+with open('cumberland1867_collection.json', 'w') as file:
+    json.dump(json.loads(dumps(cursor)), file)
+    
+cursor = orange1867_collection.find({})
+with open('orange1867_collection.json', 'w') as file:
+    json.dump(json.loads(dumps(cursor)), file)
     
 @app.route("/", methods=["GET", "POST"])
 
@@ -241,10 +312,9 @@ def transcribe_two():
 
         
         # redirect user to home page
-        return render_template('transcribe_two.html', columns=columns, columns_one_1865Louisa=columns_one_1865Louisa,
-                               columns_two_1865Louisa=columns_two_1865Louisa, columns_three_1865Louisa=columns_three_1865Louisa)
-@app.route("/transcribe_four", methods=["GET", "POST"])
-def transcribe_four():
+        return render_template('transcribe_two.html', columns=columns, columns_one_1865Louisa=columns_one_1865Louisa, columns_two_1865Louisa=columns_two_1865Louisa, columns_three_1865Louisa=columns_three_1865Louisa)
+@app.route("/transcribe_three", methods=["GET", "POST"])
+def transcribe_three():
     '''Post Method allows One Shared Story volunteer to input new records for LC Birth source into MongoDB''' 
     '''Get Method displays transcribe_four.html page, which lists the column headers from LC Birth source as input fields'''
     if request.method == "POST":
@@ -260,7 +330,7 @@ def transcribe_four():
         entry['Year'] = entry['DOB\nYEAR'].apply(str)
         lcbirth = lcbirth_collection.insert_one(entry)
         # redirect user to home page
-        return render_template('transcribe_four.html')
+        return render_template('transcribe_three.html')
 
     # if get instead of post, send to transcribe.html
     else:
@@ -283,13 +353,11 @@ def transcribe_four():
         columns_three_lcbirth = columns[2::3]
 
         # redirect user to home page
-        return render_template('transcribe_four.html', columns=columns, columns_one_lcbirth=columns_one_lcbirth,
+        return render_template('transcribe_three.html', columns=columns, columns_one_lcbirth=columns_one_lcbirth,
                                columns_two_lcbirth=columns_two_lcbirth, columns_three_lcbirth=columns_three_lcbirth)
     
-@app.route("/transcribe_five", methods=["GET", "POST"])
-def transcribe_five():
-    '''Post Method allows One Shared Story volunteer to input new records for LC Birth source into MongoDB''' 
-    '''Get Method displays transcribe_five.html page, which lists the column headers from LC Birth source as input fields'''
+@app.route("/transcribe_four", methods=["GET", "POST"])
+def transcribe_four():
     if request.method == "POST":
         entry = request.form.to_dict()
         entry['Person A'] = entry['First Name(s)'] + entry['Surname']
@@ -300,7 +368,7 @@ def transcribe_five():
         
         fluvanna1867 = fluvanna1867_collection.insert_one(entry)
         # redirect user to home page
-        return render_template('transcribe_five.html')
+        return render_template('transcribe_four.html')
 
     # if get instead of post, send to transcribe.html
     else:
@@ -323,13 +391,11 @@ def transcribe_five():
         columns_three_1867 = columns[2::3]
 
         # redirect user to home page
-        return render_template('transcribe_five.html', columns=columns, columns_one_1867=columns_one_1867,
+        return render_template('transcribe_four.html', columns=columns, columns_one_1867=columns_one_1867,
                                columns_two_1867=columns_two_1867, columns_three_1867=columns_three_1867)    
     
-@app.route("/transcribe_six", methods=["GET", "POST"])
-def transcribe_six():
-    '''Post Method allows One Shared Story volunteer to input new records for LC Birth source into MongoDB''' 
-    '''Get Method displays transcribe_five.html page, which lists the column headers from LC Birth source as input fields'''
+@app.route("/transcribe_five", methods=["GET", "POST"])
+def transcribe_five():
     if request.method == "POST":
         entry = request.form.to_dict()
         entry['Person A'] = entry['First Name(s) as written (ex Wm or Wm J)'] + ' ' + entry['Surname']
@@ -340,7 +406,7 @@ def transcribe_six():
         
         louisa1866 = louisa1866_collection.insert_one(entry)
         # redirect user to home page
-        return render_template('transcribe_six.html')
+        return render_template('transcribe_five.html')
 
     # if get instead of post, send to transcribe.html
     else:
@@ -363,17 +429,142 @@ def transcribe_six():
         columns_three_1866 = columns[2::3]
 
         # redirect user to home page
-        return render_template('transcribe_six.html', columns=columns, columns_one_1866=columns_one_1866,
+        return render_template('transcribe_five.html', columns=columns, columns_one_1866=columns_one_1866,
                                columns_two_1866=columns_two_1866, columns_three_1866=columns_three_1866)    
+
+@app.route("/transcribe_six", methods=["GET", "POST"])
+def transcribe_six():
+    if request.method == "POST":
+        entry = request.form.to_dict()
+        entry['Person A'] = entry['First Name(s) as written (ex Wm or Wm J)'] + ' ' + entry['Surname']
+        entry['Person B'] = entry['First name(s) of Employer '] + ' ' + entry['Surname of employer of male negroes']
+        entry['Source'] = '1867 Buckingham Property Tax'
+        entry['Event'] = entry['Person A'] + ' is paying taxes in the year ' + entry['Year'].apply(str)
+        entry['Role A'] = 'paid taxes for themselves and their property'
+        entry['Role B'] = 'employer of ' + entry['Person A']
+        entry['Year'] = entry['Year'].apply(str)
         
+        buckingham1867 = buckingham1867_collection.insert_one(entry)
+        # redirect user to home page
+        return render_template('transcribe_six.html')
+
+    # if get instead of post, send to transcribe.html
+    else:
+        #read in dataset
+        df = pd.read_csv("BuckinghamVA1867PPTaxAllRev1.csv")
+
+        #turn dataset into json format
+        df_json_text = df.to_json(orient="records")
+        df_json = json.loads(df_json_text)
+
+        keys = df_json[0].keys()
+
+        columns = []
+
+        for key in keys:
+            columns.append(key)
+
+        columns_one_buckingham1867 = columns[::3]
+        columns_two_buckingham1867 = columns[1::3]
+        columns_three_buckingham1867 = columns[2::3]
+
+        # redirect user to home page
+        return render_template('transcribe_six.html', columns=columns,
+                               columns_one_buckingham1867=columns_one_buckingham1867,
+                               columns_two_buckingham1867=columns_two_buckingham1867,
+                               columns_three_buckingham1867=columns_three_buckingham1867)     
+
+@app.route("/transcribe_seven", methods=["GET", "POST"])
+def transcribe_seven():
+    if request.method == "POST":
+        entry = request.form.to_dict()
+        entry['Person A'] = entry['First Name(s) as written (ex Wm or Wm J)'] + ' ' + entry['Surname']
+        entry['Person B'] = entry['First name(s) of Employer '] + ' ' + entry['Surname of employer of male negroes']
+        entry['Source'] = '1867 Cumberland Property Tax'
+        entry['Event'] = entry['Person A'] + ' is paying taxes in the year ' + entry['Year'].apply(str)
+        entry['Role A'] = 'paid taxes for themselves and their property'
+        entry['Role B'] = 'employer of ' + entry['Person A']
+        entry['Year'] = entry['Year'].apply(str)
+        
+        cumberland1867 = cumberland1867_collection.insert_one(entry)
+        # redirect user to home page
+        return render_template('transcribe_seven.html')
+
+    # if get instead of post, send to transcribe.html
+    else:
+        #read in dataset
+        df = pd.read_csv("CumberlandVA1867PPTaxRPAllrev1.csv")
+
+        #turn dataset into json format
+        df_json_text = df.to_json(orient="records")
+        df_json = json.loads(df_json_text)
+
+        keys = df_json[0].keys()
+
+        columns = []
+
+        for key in keys:
+            columns.append(key)
+
+        columns_one_cumberland1867 = columns[::3]
+        columns_two_cumberland1867 = columns[1::3]
+        columns_three_cumberland1867 = columns[2::3]
+
+        # redirect user to home page
+        return render_template('transcribe_seven.html', columns=columns,
+                               columns_one_cumberland1867=columns_one_cumberland1867,
+                               columns_two_cumberland1867=columns_two_cumberland1867,
+                               columns_three_cumberland1867=columns_three_cumberland1867)     
+    
+@app.route("/transcribe_eight", methods=["GET", "POST"])
+def transcribe_eight():
+    if request.method == "POST":
+        entry = request.form.to_dict()
+        entry['Person A'] = entry['First Name(s) as written (ex Wm or Wm J)'] + ' ' + entry['Surname']
+        entry['Person B'] = entry['First name(s) of Employer '] + ' ' + entry['Surname of employer of male negroes']
+        entry['Source'] = '1867 Orange Property Tax'
+        entry['Event'] = entry['Person A'] + ' is paying taxes in the year ' + entry['Year'].apply(str)
+        entry['Role A'] = 'paid taxes for themselves and their property'
+        entry['Role B'] = 'employer of ' + entry['Person A']
+        entry['Year'] = entry['Year'].apply(str)
+        
+        orange1867 = orange1867_collection.insert_one(entry)
+        # redirect user to home page
+        return render_template('transcribe_eight.html')
+
+    # if get instead of post, send to transcribe.html
+    else:
+        #read in dataset
+        df = pd.read_csv("OrangeVA1867PPTaxAll.csv")
+
+        #turn dataset into json format
+        df_json_text = df.to_json(orient="records")
+        df_json = json.loads(df_json_text)
+
+        keys = df_json[0].keys()
+
+        columns = []
+
+        for key in keys:
+            columns.append(key)
+
+        columns_one_orange1867 = columns[::3]
+        columns_two_orange1867 = columns[1::3]
+        columns_three_orange1867 = columns[2::3]
+
+        # redirect user to home page
+        return render_template('transcribe_eight.html', columns=columns,
+                               columns_one_orange1867=columns_one_orange1867,
+                               columns_two_orange1867=columns_two_orange1867,
+                               columns_three_orange1867=columns_three_orange1867)     
+    
+    
+       
     
 @app.route("/search", methods=["GET", "POST"])
 def search():
     '''Post Method is not significant right now. No form submit requiring post method'''
-    '''Get Method reads in the data from each of our Mongo DB collections, filters out null values, 
-    and creates a single row for each individual person, role, 
-    and source regardless of whether that person/role is A, B, or C. 
-    Ultimately returns a searchable table displaying each person in the MongoDB database'''
+    '''Get Method reads in the data from each of our Mongo DB collections, filters out null values, and creates a single row for each individual person, role, and source regardless of whether that person/role is A, B, or C. Ultimately returns a searchable table displaying each person in the MongoDB database'''
     if request.method == "POST":
         # redirect user to home page
         return render_template('search.html')
@@ -395,6 +586,15 @@ def search():
         
         louisa1866_query = louisa1866_collection.find(myquery)
         louisa1866_query = pd.DataFrame(list(louisa1866_query))
+        
+        buckingham_query = buckingham1867_collection.find(myquery)
+        buckingham_query = pd.DataFrame(list(buckingham_query))
+        
+        cumberland_query = cumberland1867_collection.find(myquery)
+        cumberland_query = pd.DataFrame(list(cumberland_query))
+        
+        orange_query = orange1867_collection.find(myquery)
+        orange_query = pd.DataFrame(list(orange_query))
         
         #fluvanna
         fluvannaA = fluvanna_query.filter(['Person A', 'Role A', 'Source', 'Event', 'Year']
@@ -422,7 +622,28 @@ def search():
         louisa1866A = louisa1866_query.filter(['Person A', 'Role A', 'Source', 'Event', 'Year']
                                               , axis=1).rename(columns = {'Person A':'Name','Role A':'Role'})
         
-        people = pd.concat([fluvannaA, fluvannaB, louisaA, louisaB, birthA, birthB, birthC, fluvanna1867A, louisa1866A]).reset_index()
+        
+        buckinghamA = buckingham_query.filter(['Person A', 'Role A', 'Source', 'Event', 'Year']
+                                      , axis=1).rename(columns = {'Person A':'Name','Role A':'Role'})
+        buckinghamB = buckingham_query.filter(['Person B', 'Role B', 'Source', 'Event', 'Year']
+                                      , axis=1).rename(columns = {'Person B':'Name','Role B':'Role'})
+        
+        
+        cumberlandA = cumberland_query.filter(['Person A', 'Role A', 'Source', 'Event', 'Year']
+                                      , axis=1).rename(columns = {'Person A':'Name','Role A':'Role'})
+        cumberlandB = cumberland_query.filter(['Person B', 'Role B', 'Source', 'Event', 'Year']
+                                      , axis=1).rename(columns = {'Person B':'Name','Role B':'Role'})
+        
+        
+        orangeA = orange_query.filter(['Person A', 'Role A', 'Source', 'Event', 'Year']
+                                      , axis=1).rename(columns = {'Person A':'Name','Role A':'Role'})
+        orangeB = orange_query.filter(['Person B', 'Role B', 'Source', 'Event', 'Year']
+                                      , axis=1).rename(columns = {'Person B':'Name','Role B':'Role'})
+        
+        
+        
+        
+        people = pd.concat([fluvannaA, fluvannaB, louisaA, louisaB, birthA, birthB, birthC, fluvanna1867A, louisa1866A, buckinghamA, buckinghamB, cumberlandA, cumberlandB, orangeA, orangeB]).reset_index()
         people = people[people["Name"].str.contains("NaN") == False] 
         people = people[people["Role"].str.contains("None") == False] 
         people = people[people["Name"].str.contains("None") == False]
@@ -463,5 +684,16 @@ def search_detail():
         mydoc5 = louisa1866_collection.find(myquery5)
         data5 = pd.DataFrame(list(mydoc5))
         
-        return render_template('search_detail.html', name=name, role=role, event=event, source=source, year=year,
-                               data=data,data2=data2,data3=data3, data4=data4, data5=data5)
+        myquery6 = {"$or" : [{"Person A" : name},{"Person B" : name}]}
+        mydoc6 = buckingham1867_collection.find(myquery6)
+        data6 = pd.DataFrame(list(mydoc6))
+        
+        myquery7 = {"$or" : [{"Person A" : name},{"Person B" : name}]}
+        mydoc7 = cumberland1867_collection.find(myquery7)
+        data7 = pd.DataFrame(list(mydoc7))
+        
+        myquery8 = {"$or" : [{"Person A" : name},{"Person B" : name}]}
+        mydoc8 = orange1867_collection.find(myquery8)
+        data8 = pd.DataFrame(list(mydoc8))
+        
+        return render_template('search_detail.html', name=name, role=role, event=event, source=source, year=year, data=data,data2=data2,data3=data3, data4=data4, data5=data5, data6=data6, data7=data7, data8=data8)
